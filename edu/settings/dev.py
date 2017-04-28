40c0d8775b97cfcdd9948eb8ea9a44588f5faa9f
Jeff <jeffdh5@gmail.com>
2017-04-28 14:53:11 -0700

    added react configs

--- edu/settings/dev.py
+++ edu/settings/dev.py
@@ -2,3 +2,13 @@ from edu.settings.base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'edu_db',
+        'USER': 'jeffdh5',
+        'HOST': '',
+        'PORT': '',
+    }
+}
