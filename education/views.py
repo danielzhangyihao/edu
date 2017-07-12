de0793db2538ceec1308a416eacb04ff67973d59
daniel zhang <zhangyihao1009@gmail.com>
2017-07-11 21:50:01 -0700

    make myCourses dynamic

--- education/views.py
+++ education/views.py
@@ -13,7 +13,7 @@ def index(request):
 
 @login_required(login_url='/login/')
 def get_courses(request):
-    myCourses = Lesson.objects.order_by('-start_date')
+    myCourses = request.user.lessons.all().order_by('-start_date')
     allCourses = Lesson.objects.order_by('-start_date')
     myCourses = serializers.serialize('json', myCourses)
     allCourses = serializers.serialize('json', allCourses)
